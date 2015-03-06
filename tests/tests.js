7b505887b929746bece092efccb017affaf2b8a6
Serdar Kuzucu <serdarkuzucu13@gmail.com>
2015-03-06 23:40:52 +0200

    improvements in unit tests

--- tests/tests.js
+++ tests/tests.js
@@ -60,3 +60,27 @@ QUnit.test("tc4 - Once Listener with Filters/Attributes Test", function(assert) 
 	
 	assert.equal(triggered, 1, "Handler must be triggered once");
 });
+
+QUnit.test("tc5 - Single handler for multiple events", function(assert) {
+	var numEventA = 0;
+	var numEventB = 0;
+	
+	jem.on(['TC5Event_A', 'TC5Event_B'], function (eventName, eventAttributes) {
+		if (eventName == 'TC5Event_A') {
+			numEventA++;
+		} else if (eventName == 'TC5Event_B') {
+			numEventB++;
+		} else {
+			assert.ok(false, 'Invalid Event Name');
+		}
+	});
+	
+	jem.fire('TC5Event_A', {name: 'hello'});
+	jem.fire('TC5Event_B', {name: 'Serdar', surname: 'Kuzucu'});
+	jem.fire('TC5Event_C', {id: 1, name: 'abcd'});
+	jem.fire('TC5Event_A', {color: 'Green', weight: '12gr'});
+	jem.fire('TC5Event_D', {id: 1, name: 'hello'});
+	
+	assert.equal(numEventA, 2, "Handler must be triggered twice for TC5Event_A");
+	assert.equal(numEventB, 1, "Handler must be triggered once for TC5Event_B");
+});
